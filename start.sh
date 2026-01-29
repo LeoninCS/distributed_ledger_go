abed44ac94c1738bc6b78b8b6efa02420a09b358
Leon <147289645+LeoninCS@users.noreply.github.com>
2026-01-29 20:44:24 +0800

    chore: 启动脚本

--- start.sh
+++ start.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONFIG_PATH="${1:-config/config.yml}"
 
 cd "$DIR"
 
+mkdir -p bin
 go build -o bin/node ./cmd
 
-./bin/node
-
+./bin/node -config "$CONFIG_PATH"
 
