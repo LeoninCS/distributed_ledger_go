3d4fae26451ba4c5b0291a07bdb07803f5b18b9c
Leon <147289645+LeoninCS@users.noreply.github.com>
2026-01-29 21:43:22 +0800

    feat: 启动时删除上次执行数据

--- start.sh
+++ start.sh
@@ -12,12 +12,14 @@ if [[ -f "$CONFIG_ARG" ]]; then
 elif [[ -f "configs/${CONFIG_ARG}.yml" ]]; then
   CONFIG_PATH="configs/${CONFIG_ARG}.yml"
 else
-  echo "找不到配置文件: $CONFIG_ARG 或 configs/${CONFIG_ARG}.yml"
+  echo "???????: $CONFIG_ARG ? configs/${CONFIG_ARG}.yml"
   exit 1
 fi
+
+rm -rf "$DIR/data" "$DIR/raft"
 
 mkdir -p bin
 go build -o bin/node ./cmd
 
-echo "使用配置: $CONFIG_PATH"
+echo "????: $CONFIG_PATH"
 ./bin/node -config "$CONFIG_PATH"
